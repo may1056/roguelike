c760a39a8f1ed8cee00e8f56061d755d00d8bee9
joversound <joon7759@gmail.com>
2023-05-06 17:49:43 +0900

    한글주석

--- Assets/1Scripts/Fade.cs
+++ Assets/1Scripts/Fade.cs
@@ -7,6 +7,9 @@ public class Fade : MonoBehaviour //서서히 사라지는 효과
     private SpriteRenderer sr;
     float fadetimegoes = 0;
 
+
+    //한글 주석 으알아ㅣ너ㅣㅇ란알미ㅏㅈ댜ㅗㅜㅜㅇ피ㅗ타제버주드숱포켜
+
     void Start()
     {
         sr = GetComponent<SpriteRenderer>();
