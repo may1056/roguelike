daa4805549f00107e0e30c3b06ad05c8212a392c
wolpertinger <may1056@naver.com>
2023-05-12 01:39:02 +0900

    메인메뉴 간단하게 만듬

---
+++ Assets/1Scripts/Mainmenu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Mainmenu : MonoBehaviour // 게임매니저 사용하려했는데 씬 달라서 오브젝트 없다고 오류띄우길래 만듬
+{
+    public GameObject Option;
+
+    public void GameExit2() // 게임 종료 버튼 - 에디터에선 실행안됨
+    {
+        Application.Quit();
+    }
+
+    public void GameStart() // 게임 시작 버튼
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
