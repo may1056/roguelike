488981e695333b5cd46383f850d2d260668eecbd
wolpertinger <may1056@naver.com>
2023-05-27 23:49:52 +0900

    story 로직 수정, 배경화면 변경 기능, 오디오 기능 추가
    1. 대화창 넘기는 방식 변경.
    마우스 클릭에서 화면 전체를 뒤덮는 투명버튼을 통해 인식하는 방식으로 변함.
    (option버튼과 동시에 눌리는 문제 해결)
    
    2. 배경화면 변경 기능 추가
    
    3. 오디오 추가
    (테스트로 언더테일, ncs 노래 넣어놓음, NEXT AUDIO (TEST) 버튼으로 노래 변경 테스트 가능)

--- Assets/1Scripts/Story.cs
+++ Assets/1Scripts/Story.cs
@@ -6,25 +6,44 @@ using UnityEngine.SceneManagement;
 
 public class Story : MonoBehaviour
 {
+    // 텍스트바 점멸하는 세모
     public Transform textbarTriangle;
-    Image trianglesr;
+    public Image trianglesr;
     bool triangleenable = true;
 
+    // 텍스트바
     public Text textbar;
     public string[] texts; // 보기 편?하게 Storymanager컴포넌트에서 입력하겠습니다
-    public int textindex = 1;
+    public int textindex = 0;
 
     public bool onOption = false; // 옵션이 켜져있는가?
 
+    // 배경화면
     public Transform background;
-    public Sprite[] backgroundimages; // 아마도 텍스트바 뒤에 뜰 그림
-    Image backgroundimage;
+    public Sprite[] backgroundimages; // 텍스트바 뒤에 뜰 그림 // 놀랍게도 이미지 컴포넌트에 들어가는 것은 스프라이트였다..
+    public Image backgroundimage;
+    public int imageindex = 0;
+
+    //배경음악
+    public AudioClip[] storyAudio;
+    public AudioSource storyAudioSource;
+    public int audioindex = 0;
+
 
 
     void Start()
     {
         trianglesr = textbarTriangle.GetComponent<Image>();
         backgroundimage = background.GetComponent<Image>();
+        storyAudioSource = GetComponent<AudioSource>();
+        
+        onOption = false;
+
+        backgroundimage.sprite = backgroundimages[imageindex];
+        textbar.text = texts[textindex];
+
+        storyAudioSource.clip = storyAudio[audioindex];
+        storyAudioSource.Play();
 
         InvokeRepeating("textbarTriangleAnimaition", 0, 1f);
     }
@@ -32,19 +51,39 @@ public class Story : MonoBehaviour
 
     void Update()
     {
-        if ((Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space)) && !onOption) textindex++; // 옵션 위에 있지 않을 때, 클릭&스페이스바 입력시 대화 넘어감
+        if (Input.GetKeyDown(KeyCode.Space)) Nextscene(); // 스페이스바 입력시 Nextscene 함수 실행
 
-        backgroundimage.sprite = backgroundimages[0];
+
+        backgroundimage.sprite = backgroundimages[imageindex];
         textbar.text = texts[textindex];
     }
 
-    public void IsclickOnButton() // 버튼을 눌렀는지 확인하는 함수, 옵션버튼 눌렀을때 텍스트가 넘어가지 않게 하는 역할
+    public void Nextscene() // onclick 용 (배경에 투명버튼 설치해놓아서 옵션 이외에 공간을 클릭하면 이 함수 실행된다.)
     {
-            if(!onOption)
-            textindex--;
+        if(!onOption)
+            {
+            textindex++;
+            imageindex++;
+            }
 
-            onOption = !onOption;
-            return;
+        
+    }
+
+    public void OptionbuttonOn()
+    {
+        onOption = true;
+    }
+
+    public void OptionbuttonOff()
+    {
+        onOption = false;
+    }
+
+    public void audiotest()
+    {
+        audioindex++;
+        storyAudioSource.clip = storyAudio[audioindex];
+        storyAudioSource.Play();
     }
     void textbarTriangleAnimaition() // 단순하게 깜빡거리는게 전부인 개선의 여지가 많아 보이는 코드,, 그냥 애니메이션으로 대체할 예정
     {
